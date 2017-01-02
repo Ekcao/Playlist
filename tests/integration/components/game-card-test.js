a8b4e9f940066dddc6215c499e2a3846615be746
Eric <caokeric@gmail.com>
2017-01-02 00:58:27 -0800

    Added game-card component.

---
+++ tests/integration/components/game-card-test.js
@@ -0,0 +1,17 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+moduleForComponent('game-card', 'Integration | Component | game card', {
+  integration: true
+});
+
+test('should be marked done when icon is clicked.', function (assert) {
+    
+});
+
+test('should delete when icon is clicked.', function (assert) {
+
+});
+
+
