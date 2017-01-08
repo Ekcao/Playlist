aad8b238176eaca1dcfc77c49ab379e1014fb991
Eric <caokeric@gmail.com>
2017-01-08 00:10:25 -0800

    Added ember-ajax.

--- app/routes/games.js
+++ app/routes/games.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        let uid = this.get('firebaseSession').uid;
+        let uid = this.get('firebaseSession').get('currentUser').uid;
         return this.store.query('game', {
             userID: uid,
             orderBy: 'title'
